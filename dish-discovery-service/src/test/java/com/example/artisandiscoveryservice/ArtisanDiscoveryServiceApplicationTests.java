61aac27a565728595cc3965286c2ef16aef4a456
Naresh Agrawal <naresh.agrawal@amantyatech.com>
2024-09-01 22:19:38 +0530

    add artisan project code

---
+++ dish-discovery-service/src/test/java/com/example/artisandiscoveryservice/ArtisanDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artisandiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisanDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
